d82747658f8c18783f4b2112e2570380c10431b4
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-06-27 21:09:09 -0300

    FEAT: Exercício D

---
+++ js/d_aumento_salarial.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('btnCalcular').addEventListener('click', function () {
+        const salario = document.getElementById('entrada').value;
+        const resultado = document.getElementById('saida');
+
+        salarioConvertido = parseInt(salario);
+
+        if (salarioConvertido <= 0) {
+            resultado.innerHTML = `O salário informado é inválido, por favor digite um salário maior que R$00,00`;
+        } else if (salarioConvertido > 1500) {
+            let reajuste = salarioConvertido * (5/100);
+            let salario_reajustado = salario + reajuste;
+            resultado.innerHTML = `Salário anterior: R$${salarioConvertido}<br>Reajuste: R$${reajuste}<br>Salário reajustado: R$${salario_reajustado}`;
+        } else if (salarioConvertido < 1000) {
+            let reajuste = salarioConvertido * (10/100);
+            let salario_reajustado = salarioConvertido + reajuste;
+            resultado.innerHTML = `Salário anterior: R$${salarioConvertido}<br>Reajuste: R$${reajuste}<br>Salário reajustado: R$${salario_reajustado}`;
+        } else {
+            let reajuste = 0
+            let salario_reajustado = salarioConvertido + reajuste
+            resultado.innerHTML = `Salário anterior: R$${salarioConvertido}<br>Reajuste: R$${reajuste}<br>Salário reajustado: R$${salario_reajustado}<br>OBS: Seu salário não sofreu reajuste!`;
+        }
+    })
+})
