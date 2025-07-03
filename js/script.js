2be9c103cd13b437121677d53189e318caffb750
BrendonJoaoDEV <brendonjoaodev@gmail.com>
2025-07-02 21:42:22 -0300

    STYLE: Correção na mensagem da terceira página

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const info_paginas = [
         {caminho: 'pages/a_par_impar.html', funcao: 'Verificar se o número é par ou impar.'},
         {caminho: 'pages/b_maior_menor_igual.html', funcao: 'Verificar o maior e menor número ou se todos são iguais.'},
-        {caminho: 'pages/c_monitoramento_velocidade.html', funcao: 'Verificar se a velocidade está abaico ou acima do limite.'},
+        {caminho: 'pages/c_monitoramento_velocidade.html', funcao: 'Verificar se a velocidade está abaixo ou acima do limite.'},
         {caminho: 'pages/d_aumento_salarial.html', funcao: 'Aplicar um aumento de acordo com o valor do salário.'},
         {caminho: 'pages/e_calculo_passagens.html', funcao: 'Calcular valor da passagem de acordo com a distância da viagem.'},
         {caminho: 'pages/f_ano_bissexto.html', funcao: 'Informa se um ano é ou não bissexto com base no calendário gregoriano.'},
